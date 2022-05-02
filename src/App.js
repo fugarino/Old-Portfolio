5fc16cbe5484c8675e59df1cf5f8e11595abd161
Christian Fugarino <christian.fugarino.123@gmail.com>
2022-05-02 18:14:30 -0400

    added sass

--- src/App.js
+++ src/App.js
@@ -1,7 +1,11 @@
-import "./App.css";
+import "./App.scss";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <h1 className="logo">Fugarino</h1>
+    </div>
+  );
 }
 
 export default App;
