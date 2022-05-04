622bb1f7f0ca68dcb34896f24e46425eacad9e35
Christian Fugarino <christian.fugarino.123@gmail.com>
2022-05-04 17:44:07 -0400

    updated the menu functionality

--- src/App.js
+++ src/App.js
@@ -1,13 +1,30 @@
+import React, { useState } from "react";
 import Nav from "./Components/Nav/Nav";
 import Menu from "./Components/Menu/Menu";
 import Hero from "./Components/Hero/Hero";
 import "./App.scss";
 
 function App() {
+  const [menuSize, setMenuSize] = useState("collapsed");
+  const [menuIcon, setMenuIcon] = useState("hamburger");
+
+  const handleClick = () => {
+    setMenuSize(menuSize === "collapsed" ? "expanded" : "collapsed");
+    setMenuIcon(menuIcon === "hamburger" ? "close" : "hamburger");
+  };
+
+  const closeMenu = (e) => {
+    console.log(e.target.className);
+    if (menuSize !== "collapsed" && e.target.className !== "expanded") {
+      setMenuSize("collapsed");
+      setMenuIcon("hamburger");
+    }
+  };
+
   return (
-    <div className="App">
+    <div className="App" onClick={closeMenu}>
       <Nav />
-      <Menu />
+      <Menu handleClick={handleClick} menuIcon={menuIcon} menuSize={menuSize} />
       <span className="tag">{"<About Me>"}</span>
       <Hero />
       <span className="tag">{"</About Me>"}</span>
--- src/Components/Menu/Menu.js
+++ src/Components/Menu/Menu.js
@@ -1,18 +1,19 @@
-import React, { useState } from "react";
+import React from "react";
 
-const Menu = () => {
-  const [menuSize, setMenuSize] = useState("collapsed");
-  const [menuIcon, setMenuIcon] = useState("hamburger");
+const Menu = ({ handleClick, menuIcon, menuSize }) => {
+  // console.log(closeMenu);
+  // const [menuSize, setMenuSize] = useState("collapsed");
+  // const [menuIcon, setMenuIcon] = useState("hamburger");
 
-  const handlClick = () => {
-    setMenuSize(menuSize === "collapsed" ? "expanded" : "collapsed");
-    setMenuIcon(menuIcon === "hamburger" ? "close" : "hamburger");
-  };
+  // const handlClick = () => {
+  //   setMenuSize(menuSize === "collapsed" ? "expanded" : "collapsed");
+  //   setMenuIcon(menuIcon === "hamburger" ? "close" : "hamburger");
+  // };
 
   return (
     <menu className="menu">
       <div className="container">
-        <button className={menuIcon} onClick={handlClick}></button>
+        <button className={menuIcon} onClick={handleClick}></button>
         <div className={menuSize}></div>
       </div>
     </menu>
