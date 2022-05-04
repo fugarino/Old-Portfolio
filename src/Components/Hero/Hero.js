be81894f14d8b50d65dca038c18d3e2650e1c3a6
Christian Fugarino <christian.fugarino.123@gmail.com>
2022-05-04 12:29:12 -0400

    fixed resizing issues on hero

--- src/Components/Hero/Hero.js
+++ src/Components/Hero/Hero.js
@@ -6,14 +6,12 @@ const Hero = () => {
     <section className="hero">
       <div className="hero-grid">
         <div className="hero-text">
-          <span className="opening-span">{"<About Me>"}</span>
           <h1>Christian Fugarino</h1>
           <h4>FRONT END DEVELOPER</h4>
           <p>
             Hi, I'm Christian. I share an immense passion for the web. <br /> Also, cats are better than dogs.
           </p>
           <button>Contact me!</button>
-          <span className="closing-span">{"</About Me>"}</span>
         </div>
         <div className="hero-img">
           <img src={Image} alt="hero"></img>
