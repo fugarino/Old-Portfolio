a30f57d309c9a4ccbea63b7ca693e9db9a5b7898
Christian Fugarino <christian.fugarino.123@gmail.com>
2022-05-04 09:59:32 -0400

    rough hero section done

--- src/Components/Hero/Hero.js
+++ src/Components/Hero/Hero.js
@@ -6,12 +6,14 @@ const Hero = () => {
     <section className="hero">
       <div className="hero-grid">
         <div className="hero-text">
+          <span className="opening-span">{"<About Me>"}</span>
           <h1>Christian Fugarino</h1>
           <h4>FRONT END DEVELOPER</h4>
           <p>
             Hi, I'm Christian. I share an immense passion for the web. <br /> Also, cats are better than dogs.
           </p>
           <button>Contact me!</button>
+          <span className="closing-span">{"</About Me>"}</span>
         </div>
         <div className="hero-img">
           <img src={Image} alt="hero"></img>
--- src/Components/Nav/Nav.js
+++ src/Components/Nav/Nav.js
@@ -3,7 +3,10 @@ import React from "react";
 const Nav = () => {
   return (
     <nav>
-      <h1 className="logo">Fugarino</h1>
+      <h1 className="logo">CRIS</h1>
+      <span className="cv-email">download cv</span>
+      <span className="divider">/</span>
+      <span className="cv-email mail">copy email</span>
     </nav>
   );
 };
