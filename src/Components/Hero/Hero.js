d27d23d526c4d095a0e25b8bb8ee47126272bc5a
Christian Fugarino <christian.fugarino.123@gmail.com>
2022-05-03 20:48:13 -0400

    hero about me section

--- src/Components/Hero/Hero.js
+++ src/Components/Hero/Hero.js
@@ -8,6 +8,9 @@ const Hero = () => {
         <div className="hero-text">
           <h1>Christian Fugarino</h1>
           <h4>FRONT END DEVELOPER</h4>
+          <p>
+            Hi, I'm Christian. I share an immense passion for the web. <br /> Also, cats are better than dogs.
+          </p>
           <button>Contact me!</button>
         </div>
         <div className="hero-img">
