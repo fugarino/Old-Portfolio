fa4bffd8f1fe02fb63629ef5fbae5f1c36f0fd22
Christian Fugarino <christian.fugarino.123@gmail.com>
2022-05-06 11:08:50 -0400

    finished skills component

--- src/Components/Skills/Skills.js
+++ src/Components/Skills/Skills.js
@@ -1,20 +1,52 @@
 import React from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faGitAlt } from "@fortawesome/free-brands-svg-icons";
 
 const Skills = () => {
   return (
     <section className="skills-section">
       <h2 className="header">Skills</h2>
       <main className="skills-grid">
-        <div className="item"></div>
-        <div className="item"></div>
-        <div className="item"></div>
-        <div className="item"></div>
-        <div className="item"></div>
-        <div className="item"></div>
-        <div className="item"></div>
-        <div className="item"></div>
-        <div className="item"></div>
-        <div className="item"></div>
+        <div className="item">
+          <i class="devicon-html5-plain colored"></i>
+          <h4>HTML</h4>
+        </div>
+        <div className="item">
+          <i class="devicon-css3-plain colored"></i>
+          <h4>CSS</h4>
+        </div>
+        <div className="item">
+          <i class="devicon-javascript-plain colored"></i>
+          <h4>JavaScript</h4>
+        </div>
+        <div className="item">
+          <i class="devicon-typescript-plain colored"></i>
+          <h4>TypeScript</h4>
+        </div>
+        <div className="item">
+          <i class="devicon-react-original colored"></i>
+          <h4>React</h4>
+        </div>
+        <div className="item">
+          <i class="devicon-redux-original colored"></i>
+          <h4>Redux</h4>
+        </div>
+        <div className="item">
+          <i class="devicon-sass-original colored"></i>
+          <h4>Sass</h4>
+        </div>
+        <div className="item">
+          <i class="devicon-tailwindcss-plain colored"></i>
+          <h4>Tailwind</h4>
+        </div>
+        <div className="item">
+          <FontAwesomeIcon icon={faGitAlt} className="git-alt" />
+          <h4>Git</h4>
+        </div>
+        <div className="item">
+          <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/figma/figma-original.svg" />
+          <h4>Figma</h4>
+        </div>
       </main>
     </section>
   );
