0369cf2020a8215d786cb07078ec9c04aab01d37
Christian Fugarino <christian.fugarino.123@gmail.com>
2022-06-06 11:50:36 -0400

    updated form

--- src/Components/Contact/Contact.js
+++ src/Components/Contact/Contact.js
@@ -3,6 +3,14 @@ import React from "react";
 const Contact = () => {
   return (
     <section className="contact-section">
+      <div className="balls">
+        <div className="ball1"></div>
+        <div className="ball2"></div>
+        <div className="ball3"></div>
+        <div className="ball4"></div>
+        <div className="ball5"></div>
+        <div className="ball6"></div>
+      </div>
       <div className="form-container">
         <div className="form-text">
           <h2>Get in touch</h2>
@@ -13,7 +21,7 @@ const Contact = () => {
           <input type="text" placeholder="Name" />
           <input type="text" placeholder="Email" />
           <textarea type="text" placeholder="Message" />
-          <button type="submit">Send message!</button>
+          <button type="submit">Submit</button>
         </form>
       </div>
     </section>
