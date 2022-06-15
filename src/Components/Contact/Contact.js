6955cc6ea24e1420198d05d5777502270a099c16
Christian Fugarino <christian.fugarino.123@gmail.com>
2022-06-15 19:42:46 -0400

    form container

--- src/Components/Contact/Contact.js
+++ src/Components/Contact/Contact.js
@@ -1,5 +1,4 @@
 import React from "react";
-import Image from "./Email2.gif";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faLinkedinIn } from "@fortawesome/free-brands-svg-icons";
 import { faGithub } from "@fortawesome/free-brands-svg-icons";
@@ -15,26 +14,7 @@ const Contact = () => {
           <FontAwesomeIcon icon={faLinkedinIn} />
         </button>
       </aside>
-      <div className="form-container">
-        <div className="form-img">
-          <img src={Image} alt="email" />
-        </div>
-        <div className="form-div">
-          <div className="form-container">
-            <div className="form-text">
-              <h2>Get in touch</h2>
-              <p>Have any question, email me.</p>
-              {/* <p>Shoot me an email.</p> */}
-            </div>
-            <form className="form">
-              <input type="text" placeholder="Name" />
-              <input type="text" placeholder="Email" />
-              <textarea type="text" placeholder="Message" />
-              <button type="submit">Submit</button>
-            </form>
-          </div>
-        </div>
-      </div>
+      <div className="contact-container"></div>
     </section>
   );
 };
