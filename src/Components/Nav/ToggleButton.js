75691645ef6b747ebda64d5204ee13d57b28acdb
Christian Fugarino <christian.fugarino.123@gmail.com>
2022-06-23 19:52:55 -0400

    lightbulb svg

--- src/Components/Nav/ToggleButton.js
+++ src/Components/Nav/ToggleButton.js
@@ -1,8 +1,13 @@
 import React from "react";
+import Image from "./lightbulb.svg";
 import "./toggleButton.scss";
 
-const ToggleButton = ({ text }) => {
-  return <button className="toggle-button">{text}</button>;
+const ToggleButton = () => {
+  return (
+    <button className="toggle-button">
+      <img src={Image} alt="lightbulb" />
+    </button>
+  );
 };
 
 export default ToggleButton;
